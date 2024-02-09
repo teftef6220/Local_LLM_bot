497fee8a2f0899f156ea0b32e1aa8fe36b616c53
teftef <hanying0226tohoku@gmail.com>
2024-02-09 22:27:58 +0900

    change precision and cfg

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import argparse
 def cfg():
     parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
     parser.add_argument('--model_name',type = str, default="rinna/japanese-gpt-neox-3.6b-instruction-sft", help='model')
-    parser.add_argument('--model_base_dir',type = str, default="../models", help='model')
+    parser.add_argument('--model_base_dir',type = str, default="./models", help='model')
     parser.add_argument('--model_instance_dir',type = str, default="result_mafuyu", help='model')
     parser.add_argument('--ckpt_name',type = str, default="model.ckpt", help='model')
 
--- llm.py
+++ llm.py
@@ -24,18 +24,27 @@ class Language_model():
         self.device = device
 
     def prepare_models(self,quantization_type,precision):
-        bnb_config = BitsAndBytesConfig(
-                    load_in_4bit=True,  # 4bitベースモデルの有効化
-                    bnb_4bit_quant_type=quantization_type,  # 量子化種別 (fp4 or nf4)
-                    bnb_4bit_compute_dtype=precision,  # 4bitベースモデルのdtype (float16 or bfloat16)
-                    bnb_4bit_use_double_quant=False,  # 4bitベースモデルのネストされた量子化の有効化 (二重量子化)
-                )
-        
-        base_model = AutoModelForCausalLM.from_pretrained(
-                    self.model_name,
-                    quantization_config=bnb_config,
-                    device_map={"": 0}
-                )
+
+        if quantization_type == "nf4":
+            bnb_config = BitsAndBytesConfig(
+                        load_in_4bit=True,  # 4bitベースモデルの有効化
+                        bnb_4bit_quant_type=quantization_type,  # 量子化種別 (fp4 or nf4)
+                        bnb_4bit_compute_dtype=precision,  # 4bitベースモデルのdtype (float16 or bfloat16)
+                        bnb_4bit_use_double_quant=False,  # 4bitベースモデルのネストされた量子化の有効化 (二重量子化)
+                    )
+            
+            base_model = AutoModelForCausalLM.from_pretrained(
+                        self.model_name,
+                        quantization_config=bnb_config,
+                        device_map={"": 0}
+                    )
+        elif quantization_type == "fp8":
+            
+            base_model = AutoModelForCausalLM.from_pretrained(
+                        self.model_name,
+                        load_in_8bit=True,
+                        device_map={"": 0}
+                    )
         
         model = PeftModel.from_pretrained(
                     base_model,
