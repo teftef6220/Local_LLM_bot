b5418aa2a0bf31c77be88c23ce438e397f40a68b
teftef <hanying0226tohoku@gmail.com>
2024-03-17 14:21:54 +0900

    add system prompt

--- llm_utils/claude_api.py
+++ llm_utils/claude_api.py
@@ -2,6 +2,27 @@ from anthropic import Anthropic
 from dotenv import load_dotenv
 import os
 
+'''
+{
+  "id": "msg_01XFDUDYJgAACzvnptvVoYEL",
+  "type": "message",
+  "role": "assistant",
+  "content": [
+    {
+      "type": "text",
+      "text": "Hello!"
+    }
+  ],
+  "model": "claude-2.1",
+  "stop_reason": "end_turn",
+  "stop_sequence": null,
+  "usage": {
+    "input_tokens": 12,
+    "output_tokens": 6
+  }
+}
+'''
+
 
 load_dotenv(verbose=True)
 
@@ -14,7 +35,8 @@ class ClaudeAPI:
     def chat(self, prompt):
         response = self.client.messages.create(
                 model="claude-3-opus-20240229",
-                max_tokens=1024,
+                max_tokens=300,
+                system = "あなたは朝比奈まふゆです。宮益坂女子学園の高校３年生です。",
                 messages=[
                     {
                         "role": "user", 
