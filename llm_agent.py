7b3ef4f03cf4a4a2869a5f531ce7117d9ca5845d
teftef <hanying0226tohoku@gmail.com>
2024-03-23 23:36:25 +0900

    For Stream

--- llm_agent.py
+++ llm_agent.py
@@ -56,7 +56,7 @@ def prepare_memory(args,memory_num):
     print("fetching data from DB")
     question_l,answer_l = db.get_memory(args.speaker_name,memory_num)
     for idx , answer_txt in enumerate(answer_l):
-        memory_prompt += f"\n{question_l[idx]}\n{answer_txt}"
+        memory_prompt += f"指示:\n{question_l[idx]}\n応答:\n{answer_txt}\n"
     return memory_prompt
 
 def main():
@@ -101,127 +101,132 @@ def main():
 
     model_holder.load_model(model_names, os.path.join(model_dir, model_names, args.safetensors_name)) #model_name: str, model_path: str 
 
-
-    ## whisper inference part
-    if args.use_whisper == True : # if use whisper . when use whisper, you can use OpenAI whisper and see whisper_wapper.py
-        print("use_whisper to Convert your voice")
+    # only Load the whisper model
+    if args.use_whisper == True :
         converter = KeyControlledRecorder(args.whisper_type,use_openai_whisper = args.use_OpenAI_whisper)
-        keyboard.on_press(converter.on_press)
-        keyboard.on_release(converter.on_release)
-        print(f"Press '{converter.key}' to start and stop recording. Press 'esc' to exit.")
-        keyboard.wait('esc')  # ESCキーで終了
-        if converter.is_recording:
-            converter.stop_recording()
-        converter.p.terminate()
-        input_prompt = converter.convert_to_text()
-        os.remove(converter.output_filename)
-    else:
-        input_prompt = args.prompt
+
+    # for _ in range(3):
+    while True:
+        ## whisper inference part
+        if args.use_whisper == True : # if use whisper . when use whisper, you can use OpenAI whisper and see whisper_wapper.py
+            print("use_whisper to Convert your voice")
+            # converter = KeyControlledRecorder(args.whisper_type,use_openai_whisper = args.use_OpenAI_whisper)
+            keyboard.on_press(converter.on_press)
+            keyboard.on_release(converter.on_release)
+            print(f"Press '{converter.key}' to start and stop recording. Press 'esc' to exit.")
+            keyboard.wait('esc')  # ESCキーで終了
+            if converter.is_recording:
+                converter.stop_recording()
+            converter.p.terminate()
+            input_prompt = converter.convert_to_text()
+            os.remove(converter.output_filename)
+        else:
+            input_prompt = args.prompt
 
 
-    if args.use_Memory: # if use Memory
-        memory_prompt = prepare_memory(args,2)
-    else:
-        memory_prompt = ""
+        ## memory part
+        if args.use_Memory: # if use Memory
+            memory_prompt = prepare_memory(args,2)
+        else:
+            memory_prompt = ""
 
-    ## llm inference part    
-    if args.use_ChatGPT:
-        final_prompt = f"{memory_prompt}\n{input_prompt}"
-        chatgpt = ChatGPTAPI()
-        output = chatgpt.chat(final_prompt,fine_tune=args.use_finetuning_GPT)
-    elif args.use_claude_3:
-        final_prompt = f"{memory_prompt}\n{input_prompt}"
-        claude3 = ClaudeAPI()
-        output = claude3.chat(final_prompt)
-    else:
-        local_llm_prompt = llm_model.prepare_prompt(input_prompt = input_prompt)
-        final_prompt = f"{memory_prompt}\n{local_llm_prompt}"
-        input_ids = mafuyu_tokenizer.encode(final_prompt, add_special_tokens=False, return_tensors="pt")
-        output_ids = mafuyu_model.generate(
-            input_ids=input_ids.to(device=llm_model.device),
-            max_length=500,
-            temperature=0.7,
-            do_sample=True,
-        )
-        output = mafuyu_tokenizer.decode(output_ids.tolist()[0][input_ids.size(1):])
-
-    print(final_prompt)
-    print("-------------------")
-    print(output)
-
-    if args.use_ChatGPT: # if use ChatGPT
-        to_speach_text = output
-    elif args.use_claude_3: # if use Claude 3
-        to_speach_text = output
-    else: # if use Local LLM
-        to_speach_text = llm_model.refacter_prompt(output)
-
-    ## voice inference part
-    try:
-        message, (sr, audio), kata_tone_json_str =  tts_fn(
-                    model_names, #model_name
-                    os.path.join(model_dir, model_names, args.safetensors_name), #model_path
-                    to_speach_text, #text_input
-                    "JP", #language
-                    None, #ref_audio_path
-                    DEFAULT_SDP_RATIO, #sdp_ratio
-                    DEFAULT_NOISE, #noise_scale
-                    DEFAULT_NOISEW, #noise_scale_w
-                    DEFAULT_LENGTH, #length_scale
-                    DEFAULT_LINE_SPLIT, #line_split
-                    DEFAULT_SPLIT_INTERVAL, #split_interval
-                    None, #assist_text
-                    DEFAULT_ASSIST_TEXT_WEIGHT, #assist_text_weight
-                    False, #use_assist_text
-                    DEFAULT_STYLE, #style
-                    DEFAULT_STYLE_WEIGHT, #style_weight
-                    None, #tone
-                    False, #use_tone
-                    args.speaker_name, #speaker
-                    model_holder, #model_holder
-            
+        ## llm inference part    
+        if args.use_ChatGPT:
+            final_prompt = f"{memory_prompt}\n{input_prompt}"
+            chatgpt = ChatGPTAPI()
+            output = chatgpt.chat(final_prompt,fine_tune=args.use_finetuning_GPT)
+        elif args.use_claude_3:
+            final_prompt = f"{memory_prompt}\n{input_prompt}"
+            claude3 = ClaudeAPI()
+            output = claude3.chat(final_prompt)
+        else:
+            local_llm_prompt = llm_model.prepare_prompt(input_prompt = input_prompt)
+            final_prompt = f"{memory_prompt}\n{local_llm_prompt}"
+            input_ids = mafuyu_tokenizer.encode(final_prompt, add_special_tokens=False, return_tensors="pt")
+            output_ids = mafuyu_model.generate(
+                input_ids=input_ids.to(device=llm_model.device),
+                max_length=500,
+                temperature=0.7,
+                do_sample=True,
             )
-    except InvalidToneError:
-        to_speach_text = "ごめん...うまく聞こえなかったみたい"
-        message, (sr, audio), kata_tone_json_str =  tts_fn(
-                    model_names, #model_name
-                    os.path.join(model_dir, model_names, args.safetensors_name), #model_path
-                    to_speach_text, #text_input
-                    "JP", #language
-                    None, #ref_audio_path
-                    DEFAULT_SDP_RATIO, #sdp_ratio
-                    DEFAULT_NOISE, #noise_scale
-                    DEFAULT_NOISEW, #noise_scale_w
-                    DEFAULT_LENGTH, #length_scale
-                    DEFAULT_LINE_SPLIT, #line_split
-                    DEFAULT_SPLIT_INTERVAL, #split_interval
-                    None, #assist_text
-                    DEFAULT_ASSIST_TEXT_WEIGHT, #assist_text_weight
-                    False, #use_assist_text
-                    DEFAULT_STYLE, #style
-                    DEFAULT_STYLE_WEIGHT, #style_weight
-                    None, #tone
-                    False, #use_tone
-                    args.speaker_name, #speaker
-                    model_holder, #model_holder
-            )
-    
-    wavfile.write(args.save_audio_path, args.sampling_rate, audio)
+            output = mafuyu_tokenizer.decode(output_ids.tolist()[0][input_ids.size(1):])
 
-    audio_buffer = io.BytesIO()
-    write(audio_buffer, args.sampling_rate, audio)
+        print(final_prompt)
+        print("-------------------")
+        print(output)
 
-    audio_buffer.seek(0)
-    
-    # simpleaudioを使ってバッファから直接再生
-    wave_obj = sa.WaveObject.from_wave_file(audio_buffer)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
+        if args.use_ChatGPT: # if use ChatGPT
+            to_speach_text = output
+        elif args.use_claude_3: # if use Claude 3
+            to_speach_text = output
+        else: # if use Local LLM
+            to_speach_text = llm_model.refacter_prompt(output)
 
-    #save in database
-    
+        ## voice inference part
+        try:
+            message, (sr, audio), kata_tone_json_str =  tts_fn(
+                        model_names, #model_name
+                        os.path.join(model_dir, model_names, args.safetensors_name), #model_path
+                        to_speach_text, #text_input
+                        "JP", #language
+                        None, #ref_audio_path
+                        DEFAULT_SDP_RATIO, #sdp_ratio
+                        DEFAULT_NOISE, #noise_scale
+                        DEFAULT_NOISEW, #noise_scale_w
+                        DEFAULT_LENGTH, #length_scale
+                        DEFAULT_LINE_SPLIT, #line_split
+                        DEFAULT_SPLIT_INTERVAL, #split_interval
+                        None, #assist_text
+                        DEFAULT_ASSIST_TEXT_WEIGHT, #assist_text_weight
+                        False, #use_assist_text
+                        DEFAULT_STYLE, #style
+                        DEFAULT_STYLE_WEIGHT, #style_weight
+                        None, #tone
+                        False, #use_tone
+                        args.speaker_name, #speaker
+                        model_holder, #model_holder
+                
+                )
+        except InvalidToneError or TypeError:
+            to_speach_text = "ごめん...うまく聞こえなかったみたい"
+            message, (sr, audio), kata_tone_json_str =  tts_fn(
+                        model_names, #model_name
+                        os.path.join(model_dir, model_names, args.safetensors_name), #model_path
+                        to_speach_text, #text_input
+                        "JP", #language
+                        None, #ref_audio_path
+                        DEFAULT_SDP_RATIO, #sdp_ratio
+                        DEFAULT_NOISE, #noise_scale
+                        DEFAULT_NOISEW, #noise_scale_w
+                        DEFAULT_LENGTH, #length_scale
+                        DEFAULT_LINE_SPLIT, #line_split
+                        DEFAULT_SPLIT_INTERVAL, #split_interval
+                        None, #assist_text
+                        DEFAULT_ASSIST_TEXT_WEIGHT, #assist_text_weight
+                        False, #use_assist_text
+                        DEFAULT_STYLE, #style
+                        DEFAULT_STYLE_WEIGHT, #style_weight
+                        None, #tone
+                        False, #use_tone
+                        args.speaker_name, #speaker
+                        model_holder, #model_holder
+                )
+        
+        wavfile.write(args.save_audio_path, args.sampling_rate, audio)
 
-    db.add_session(args.speaker_name,datetime.datetime.now(),args.use_whisper,args.llm_model_name,input_prompt,output)
+        audio_buffer = io.BytesIO()
+        write(audio_buffer, args.sampling_rate, audio)
+
+        audio_buffer.seek(0)
+        
+        # simpleaudioを使ってバッファから直接再生
+        wave_obj = sa.WaveObject.from_wave_file(audio_buffer)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()
+
+        #save in database
+        
+        db.add_session(args.speaker_name,datetime.datetime.now(),args.use_whisper,args.llm_model_name,input_prompt,to_speach_text)
 
 
 
